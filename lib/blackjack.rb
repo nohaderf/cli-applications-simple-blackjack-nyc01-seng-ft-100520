e2ccb85709d5cceb92a04959c62b944dfa419ea9
Freda Hon <freda.hon@gmail.com>
2020-09-17 16:53:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -71,10 +71,10 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
-  if get_user_input == "h"
+  user_input = get_user_input
+  if user_input == "h"
     card_total += deal_card
-  elsif get_user_input == "s"
+  elsif user_input == "s"
     card_total
   else
     invalid_command
